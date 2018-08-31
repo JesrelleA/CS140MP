1b05bc1f80257962a97c45ea4bb601801e49f297
rpscruz <rscruz7@up.edu,ph>
2018-08-31 17:37:05 +0800

    HTML table formatted

--- MP.py
+++ MP.py
@@ -1,4 +1,42 @@
-filename = "hello.txt"
+filename = "recipe.txt"
 file = open(filename, "r")
 for line in file:
-   print line,
+   print(line)
+
+task = {"adob", "sinigang", "tapsilog"} 
+
+#html = """<html><table border="1">
+#<tr><th>Time</th><th>Cook</th><th>Ready</th><th>Assistants</th></tr>"""
+
+clock = 0;
+headers = ["Time", "Cook", "Ready", "Assistants", "Remarks"]
+html = """<html><table border="1">
+<tr>
+    <th>Time</th>
+    <th>Cook</th>
+    <th>Ready</th>
+    <th>Assistants</th>
+    <th>Remarks</th>
+</tr>"""
+
+for luto in task:
+    #html += "<tr><td>{}</td>".format(task)
+    for state in headers:
+        if state == "Time":
+            html += "<td>"+str(clock)+"</td>"
+            clock+=1
+        else:
+            html += "<td>{}</td>".format(state)
+
+        #"<td>{}</td>".format('<br>'.join("laman"))
+
+    html += "</tr>"
+
+html += "</table></html>"
+
+
+#print(html)
+
+f = open("output.html", "w+")
+f.write(html)
+f.close()
