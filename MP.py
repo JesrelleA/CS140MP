0cc787ff718035536f9455753e4a7a83365ca604
rpscruz <rscruz7@up.edu.ph>
2018-09-01 22:08:13 +0800

    with recipe files

--- MP.py
+++ MP.py
@@ -8,16 +8,14 @@ task = {"adob", "sinigang", "tapsilog"}
 #html = """<html><table border="1">
 #<tr><th>Time</th><th>Cook</th><th>Ready</th><th>Assistants</th></tr>"""
 
-clock = 0;
+
 headers = ["Time", "Cook", "Ready", "Assistants", "Remarks"]
-html = """<html><table border="1">
-<tr>
-    <th>Time</th>
-    <th>Cook</th>
-    <th>Ready</th>
-    <th>Assistants</th>
-    <th>Remarks</th>
-</tr>"""
+clock = 0;
+
+html = "<html><table border=\"1\">"     # formats header
+for state in headers:
+    html += "<td>{}</td>".format(state)
+html += "</tr>"
 
 for luto in task:
     #html += "<tr><td>{}</td>".format(task)
