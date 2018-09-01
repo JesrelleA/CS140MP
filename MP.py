6011591f04571b1a47228e672cf518d335fb29e3
rpscruz <rscruz7@up.edu.ph>
2018-09-02 00:40:51 +0800

    reads recipe

--- MP.py
+++ MP.py
@@ -1,56 +1,61 @@
 filename = "tasklist.txt"
 file = open(filename, "r")
 
-recipe       = {}
-recipe_count = {}
+tasks          = {}
+tasks_count       = {}
 
 for line in file:
     task_i       = line.split()[0]
     task_sched_i = int(line.split()[1])
 
-    if task_i not in recipe_count:	# for 1st encounter with task
-        recipe_count.update({task_i : 1})
-        recipe.update({ task_sched_i : str(task_i+"1")})
-    else:							# for all subsequent encounters
-        recipe_count.update({task_i : recipe_count[task_i]+1})
-        task_v = task_i + str(recipe_count[task_i])
-        recipe.update({ task_sched_i : task_v})
+    if task_i not in tasks_count:    # for 1st encounter with task
+        tasks_count.update({task_i : 1})
+        tasks.update({ task_sched_i : str(task_i+"1")})
+    else:                            # for all subsequent encounters
+        tasks_count.update({task_i : tasks_count[task_i]+1})
+        task_v = task_i + str(tasks_count[task_i])
+        tasks.update({ task_sched_i : task_v})
 
-print("count was:", recipe_count)
-print("task list is", recipe)
-    #print(recipe_count)
-    
-    # print(recipe, time)
-    #print(line)
-task = {"adob", "sinigang", "tapsilog"} 
 
-#html = """<html><table border="1">
-#<tr><th>Time</th><th>Cook</th><th>Ready</th><th>Assistants</th></tr>"""
-
+print("count was:", tasks_count)
+print("task list is", tasks)
 
 headers = ["Time", "Cook", "Ready", "Assistants", "Remarks"]
-clock = 0;
+clock = 1;
 
 html = "<html><table border=\"1\">"     # formats header
 for state in headers:
     html += "<td>{}</td>".format(state)
 html += "</tr>"
-
-for luto in task:
+prio_queue = "none"
+for luto in tasks:
     #html += "<tr><td>{}</td>".format(task)
     for state in headers:
         if state == "Time":
-            html += "<td>"+str(clock)+"</td>"
+            html += "<td>{}</td>".format( str(clock) )
             clock+=1
+        elif state == "Cook":
+            task_filename = ''.join(i for i in tasks[luto] if not i.isdigit()) + ".txt"
+            task_file = open(task_filename, "r")
+
+            cooking = task_filename
+
+            for line in task_file:
+            	cooking += "\n " + line
+
+            """
+            if ()
+            if (prio_queue == "none"):
+                cooking = tasks[luto]
+                html += "<td>{}</td>".format( cooking )
+            """
+            html += "<td>{}</td>".format( cooking )
         else:
-            html += "<td>{}</td>".format(state)
-
+            html += "<td>{}</td>".format("")
         #"<td>{}</td>".format('<br>'.join("laman"))
-
     html += "</tr>"
-
 html += "</table></html>"
 
-f = open("output.html", "w+")
-f.write(html)
-f.close()
+output = open("output.html", "w+")
+output.write(html)
+output.close()
