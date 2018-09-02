811a2b304a245df10af9aeeee39fbd488e643834
rpscruz <rscruz7@up.edu,ph>
2018-09-02 12:01:03 +0800

    loads but overwrites, no queue implementation yet

--- MP.py
+++ MP.py
@@ -1,3 +1,5 @@
+from queue import *
+
 filename = "tasklist.txt"
 file = open(filename, "r")
 
@@ -27,9 +29,11 @@ for line in file:
         task_v = task_i + str(tasks_count[task_i])
         tasks.update({ task_sched_i : task_v})
 
-
+"""
 print("count was:", tasks_count)
 print("task list is", tasks)
+
+"""
 
 headers = ["Time", "Cook", "Ready", "Assistants", "Remarks"]
 clock = 1;
@@ -40,23 +44,30 @@ for state in headers:
     html += "<td>{}</td>".format(state)
 html += "</tr>"
 
-prio_queue = "none"
-
 default = "none"
 done_i = None
 done_all = False;
+
+head      = 0
+step_index      = 0
+step_time_index = 1
 # while not done: 
 
+cooking    = ""
+ready      = ""
+assistants = "" #background tasks
+to_cook     = {}
+queue      = []
+
+
 while clock <= max(tasks, key=int):
-    cooking = ""
-    ready = ""
-    assistants = "" #background tasks
-    remarks = ""
+    remarks    = ""
+    recipe = [] # needs to be cleared every clock; but needs to be accessed beyond fetching function
 
     # chceks arrival times of tasks and fetches recipe
     if clock in tasks: 
-        recipe = []
         remarks = tasks[clock] + " arrives"
+        queue.append(tasks[clock])
 
 
         task_filename = ''.join(i for i in tasks[clock] if not i.isdigit()) + ".txt"
@@ -66,49 +77,51 @@ while clock <= max(tasks, key=int):
         for line in task_file:
             recipe.append([])
             
-            step       = line.split()[0]      # gets step at a time
-            step_time  = int(line.split()[1]) # gets duration of step
+            step       =     line.split()[step_index]      # gets step at a time
+            step_time  = int(line.split()[step_time_index]) # gets duration of step
             recipe[line_num].append(step)
             recipe[line_num].append(step_time)
 
             line_num += 1
+          
+        to_cook[tasks[clock]] = recipe
+        #print(recipe)
+        #print(to_cook)
 
-        print(recipe)
+        # to_cook.update({tasks[clock]: recipe})
 
         if recipe: # recipe is not empty
             if cooking == "":
-                
-                cooking = str(recipe[0][0]) +"="+ str(recipe[0][1]) 
-                recipe[0][1]=recipe[0][1]-1
+                cooking =  '{}({}={})'.format(tasks[clock], recipe[head][step_index], recipe[head][step_time_index])
+                recipe[head][step_time_index] = recipe[head][step_time_index] - 1
                 #cooking = ' '.join(map(str, recipe))
+            elif not cooking == "":
+                cooking =  '{}({}={})'.format(tasks[clock], recipe[head][step_index], recipe[head][step_time_index])
+                recipe[head][step_time_index] = recipe[head][step_time_index] - 1
+
+               #remarks += " something is already cooking"
             else:
-                ready = "smth"
+                ready = cooking =  '{}({}={})'.format(tasks[clock], recipe[head][step_index], recipe[head][step_time_index])
+
+
+                """
+                if recipe[head][step_time_index] == 0:
+                cooking = ""
+                """
+
+
+
 
 
     for state in headers:
         if state == "Time":
             html += "<td>{}</td>".format( str(clock) )
             clock += 1
-        elif state == "Cook":
-
-#            print(tasks[clock], recipe)
-
-            """
-            if ()
-            if (prio_queue == "none"):
-                cooking = tasks[luto]
-                html += "<td>{}</td>".format( cooking )
-            """
-            html += "<td>{}</td>".format( cooking )
-        elif state == "Ready":
-            html += "<td>{}</td>".format( ready )
-        elif state == "Assistants":
-            html += "<td>{}</td>".format( assistants )
-
-        elif state == "Remarks":
-            html += "<td>{}</td>".format( remarks )
-        else:
-            html += "<td>{}</td>".format( default )
+        elif state == "Cook":       html += "<td>{}</td>".format( cooking )
+        elif state == "Ready":      html += "<td>{}</td>".format( ready )
+        elif state == "Assistants": html += "<td>{}</td>".format( assistants )
+        elif state == "Remarks":    html += "<td>{}</td>".format( remarks )
+        else:                       html += "<td>{}</td>".format( default )
         #"<td>{}</td>".format('<br>'.join("laman"))
     html += "</tr>"
 
@@ -116,6 +129,17 @@ while clock <= max(tasks, key=int):
         done = True
 
 html += "</table></html>"
+
+style = """
+<style>
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+</style>
+"""
+
+html += style
 
 output = open("output.html", "w+")
 output.write(html)
--- readme.txt
+++ readme.txt
@@ -1,3 +1,5 @@
 machine problem for up diliman cs 140 (operating systems) course under prof. mario carreon, for academic year 2018-2019, first semester
 
 due sept 10
+
+Brianna patricia cru
